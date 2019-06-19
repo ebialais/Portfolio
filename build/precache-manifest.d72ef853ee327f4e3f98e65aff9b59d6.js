3810e3544218b05375c8ce18ffae62ac138aebc4
Emilie Bialais <ebialais@gmail.com>
2019-06-19 13:40:54 +0200

    change CV

---
+++ build/precache-manifest.d72ef853ee327f4e3f98e65aff9b59d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84936b0da3fabd6df67ac380de19cfa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce5e4c90b006ec19300e",
+    "url": "/static/css/main.ac49924c.chunk.css"
+  },
+  {
+    "revision": "be1a4f89ff3b9302f9e2",
+    "url": "/static/js/2.570852e1.chunk.js"
+  },
+  {
+    "revision": "ce5e4c90b006ec19300e",
+    "url": "/static/js/main.837ea456.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
